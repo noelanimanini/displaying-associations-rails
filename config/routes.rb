4bd0958b97896aacb29e99981bad2a8e4b86dff9
smolnreadytobrawl <noelani.villanueva@gmail.com>
2020-12-16 10:51:56 -0800

    wishing i could run like naruto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories, :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :categories, :categories, only: [:index, :show, :new, :create, :edit, :update]
+  resources :categories, :authors, only: [:index, :show, :new, :create, :edit, :update]
 end
